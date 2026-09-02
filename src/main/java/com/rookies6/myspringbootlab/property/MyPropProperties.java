8bcdabed192ab99034aa3657a3609e7c70989da8
jero-06 <jeeyoung345@gmail.com>
2026-09-02 23:27:52 +0900

    MyPropProperties add

---
+++ src/main/java/com/rookies6/myspringbootlab/property/MyPropProperties.java
@@ -0,0 +1,10 @@
+package com.rookies6.myspringbootlab.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties
+public class MyPropProperties {
+
+}
