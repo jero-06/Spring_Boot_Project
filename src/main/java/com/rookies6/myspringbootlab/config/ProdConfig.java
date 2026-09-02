6f23e2eb5afaf76988675d19ccf7fb9b5bd074d3
jero-06 <jeeyoung345@gmail.com>
2026-09-03 00:35:33 +0900

    property add

--- src/main/java/com/rookies6/myspringbootlab/config/ProdConfig.java
+++ src/main/java/com/rookies6/myspringbootlab/config/ProdConfig.java
@@ -1,11 +1,13 @@
 package com.rookies6.myspringbootlab.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("prod")
 public class ProdConfig {
+    @Bean
     public MyEnvironment myEnvironment() {
         return MyEnvironment.builder()
                 .mode("운영환경")
