2d94fd1d09884932ba103840eec37465eeda42cc
jero-06 <jeeyoung345@gmail.com>
2026-09-03 00:18:02 +0900

    MyRunner update

--- src/main/java/com/rookies6/myspringbootlab/property/MyPropProperties.java
+++ src/main/java/com/rookies6/myspringbootlab/property/MyPropProperties.java
@@ -1,10 +1,14 @@
 package com.rookies6.myspringbootlab.property;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties
+@ConfigurationProperties("myprop")
+@Getter @Setter
 public class MyPropProperties {
-
-}
+    private String username;
+    private int port;
+    }
--- src/main/java/com/rookies6/myspringbootlab/runner/MyPropRunner.java
+++ src/main/java/com/rookies6/myspringbootlab/runner/MyPropRunner.java
@@ -1,22 +1,39 @@
 package com.rookies6.myspringbootlab.runner;
 
+import com.rookies6.myspringbootlab.property.MyPropProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyPropRunner implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(MyPropRunner.class);
+
     @Value("${myprop.username}")
     private String username;
 
     @Value("${myprop.port}")
     private int port;
 
+    private final MyPropProperties myPropProperties;
+
+       public MyPropRunner(MyPropProperties myPropProperties) {
+        this.myPropProperties = myPropProperties;
+
+    }
+
     @Override
     public void run(String... args) throws Exception {
 
         System.out.println("Myprop username = " + username);
         System.out.println("Myprop int = " + port);
+
+        logger.info("username = {}", myPropProperties.getUsername());
+        logger.info("port = {}", myPropProperties.getPort());
     }
 
 }
