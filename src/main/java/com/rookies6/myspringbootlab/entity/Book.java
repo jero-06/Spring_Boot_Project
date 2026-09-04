6f966ac4c29f39cee839445f4d486e9c4c074955
jero-06 <jeeyoung345@gmail.com>
2026-09-04 16:33:53 +0900

    StudentDTO add

--- src/main/java/com/rookies6/myspringbootlab/entity/Book.java
+++ src/main/java/com/rookies6/myspringbootlab/entity/Book.java
@@ -1,10 +1,7 @@
 package com.rookies6.myspringbootlab.entity;
 
 import io.micrometer.core.instrument.Meter;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -17,7 +14,7 @@ import java.time.LocalDate;
 public class Book {
 
     @Id
-    @GeneratedValue(strategy = Long Id)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
 
     private String title;
