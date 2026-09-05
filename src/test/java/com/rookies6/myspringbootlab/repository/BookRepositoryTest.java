6879b0ac40a1860f88657d1d5b3fd194b25a19ef
jero-06 <jeeyoung345@gmail.com>
2026-09-05 19:16:20 +0900

    mysql add

--- src/test/java/com/rookies6/myspringbootlab/repository/BookRepositoryTest.java
+++ src/test/java/com/rookies6/myspringbootlab/repository/BookRepositoryTest.java
@@ -4,9 +4,11 @@ import com.rookies6.myspringbootlab.entity.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.Optional;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,7 +24,7 @@ public class BookRepositoryTest {
         Book book = Book.builder()
                 .title("스프링 부트 입문")
                 .author("홍길동")
-                .isbn("9788956746425")
+                .isbn("0000000000000")
                 .price(30000)
                 .publishDate(LocalDate.of(2025,05,07))
                 .build();
@@ -37,22 +39,22 @@ public class BookRepositoryTest {
     // ISBN으로 도서 조회 테스트 ( testFindByIsbn() )
     @Test
     void testFindByIsbn() {
-        // Given(준비단계)
-        Book book = Book.builder()
-                .title("JPA 프로그래밍")
-                .author("박둘리")
-                .isbn("9788956746432")
-                .price(35000)
-                .publishDate(LocalDate.of(2025, 4, 30))
-                .build();
-        bookRepository.save(book);
-
         // When(실행단계): isbn으로 조회
         Optional<Book> foundBook = bookRepository.findByIsbn("9788956746432");
 
         // Then(검증단계)
         assertTrue(foundBook.isPresent());
         assertEquals("JPA 프로그래밍", foundBook.get().getTitle());                    // Optional 안의 Book 객체에서 author를 꺼내서 비교
+    }
+
+    // 저자명으로 도서 목록 조회 테스트 ( testFindByAuthor() )
+    @Test
+    void testFindByAuthor() {
+        // When(실행단계): 저자명으로 목록 조회
+        Optional<Book> foundBooks = bookRepository.findByAuthor("홍길동");
+
+        // Then(검증단계)
+        assertEquals("홍길동", foundBooks.size());
     }
 }
 
