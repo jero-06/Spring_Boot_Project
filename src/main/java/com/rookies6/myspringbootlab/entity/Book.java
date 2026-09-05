7703f38abb36c49960fde0a3977a14bd57954fcc
jero-06 <jeeyoung345@gmail.com>
2026-09-05 15:42:30 +0900

    Book update

--- src/main/java/com/rookies6/myspringbootlab/entity/Book.java
+++ src/main/java/com/rookies6/myspringbootlab/entity/Book.java
@@ -1,4 +1,29 @@
 package com.rookies6.myspringbootlab.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+
+    @Column(unique = true)
+    private String isbn;
+
+    private LocalDate publishDate;
+    private Integer price;
+
+
 }
