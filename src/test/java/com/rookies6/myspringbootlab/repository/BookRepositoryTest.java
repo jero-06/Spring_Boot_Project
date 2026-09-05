ac966afe17c9650d1cee465c7b58d57ffcd8e242
jero-06 <jeeyoung345@gmail.com>
2026-09-05 17:38:42 +0900

    testFindByIsbn()

--- src/test/java/com/rookies6/myspringbootlab/repository/BookRepositoryTest.java
+++ src/test/java/com/rookies6/myspringbootlab/repository/BookRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,7 +37,23 @@ public class BookRepositoryTest {
     // ISBN으로 도서 조회 테스트 ( testFindByIsbn() )
     @Test
     void testFindByIsbn() {
+        // Given(준비단계)
+        Book book = Book.builder()
+                .title("JPA 프로그래밍")
+                .author("박둘리")
+                .isbn("9788956746432")
+                .price(35000)
+                .publishDate(LocalDate.of(2025, 4, 30))
+                .build();
+        bookRepository.save(book);
 
+        // When(실행단계): isbn으로 조회
+        Optional<Book> foundBook = bookRepository.findByIsbn("9788956746432");
+
+        // Then(검증단계)
+        assertTrue(foundBook.isPresent());
+        assertEquals("박둘리", book.getAuthor());                    // Optional 안의 Book 객체에서 author를 꺼내서 비교
+    }
     }
 
-}
+
