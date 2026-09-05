e23f07329cc1e331d0b567e1932012ecea54f144
jero-06 <jeeyoung345@gmail.com>
2026-09-05 16:05:46 +0900

    Bookrepository update

--- src/main/java/com/rookies6/myspringbootlab/repository/BookRepository.java
+++ src/main/java/com/rookies6/myspringbootlab/repository/BookRepository.java
@@ -1,4 +1,14 @@
 package com.rookies6.myspringbootlab.repository;
 
-public class BookRepository {
+import com.rookies6.myspringbootlab.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findByIsbn(String isbn);
+
+    List<Book> findByAuthor(String author);
+
 }
