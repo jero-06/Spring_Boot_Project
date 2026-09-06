eada6dbea6add40f9d91a328126bdcfa662426d8
jero-06 <jeeyoung345@gmail.com>
2026-09-06 14:11:33 +0900

    BusinessException add

--- src/main/java/com/rookies6/myspringbootlab/exception/ErrorObject.java
+++
@@ -1,4 +0,0 @@
-package com.rookies6.myspringbootlab.exception;
-
-public class ErrorObject {
-}
--- src/main/java/com/rookies6/myspringbootlab/exception/advice/DefaultExceptionAdvice.java
+++
@@ -1,4 +0,0 @@
-package com.rookies6.myspringbootlab.exception.advice;
-
-public class DefaultExceptionAdvice {
-}
