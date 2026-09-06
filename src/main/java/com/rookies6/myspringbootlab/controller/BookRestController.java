f49b5b733cd626e26db7c1efdf965a972913391b
jero-06 <jeeyoung345@gmail.com>
2026-09-06 15:11:22 +0900

    GetMapping Id

--- src/main/java/com/rookies6/myspringbootlab/controller/BookRestController.java
+++ src/main/java/com/rookies6/myspringbootlab/controller/BookRestController.java
@@ -2,6 +2,7 @@ package com.rookies6.myspringbootlab.controller;
 
 import com.rookies6.myspringbootlab.entity.Book;
 import com.rookies6.myspringbootlab.repository.BookRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,17 +17,26 @@ public class BookRestController {
         this.bookRepository = bookRepository;
     }
 
+    // 새 도서 등록
     @PostMapping
     public Book createBook(@RequestBody Book book) {
                 return bookRepository.save(book);
     }
 
+    // 모든 도서 조회
     @GetMapping
     public List<Book> getAllBook() {
         return bookRepository.findAll();
     }
 
+    // ID로 특정 도서 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getUserById(@PathVariable Long id) {
 
+        return bookRepository.findById(id)
+                .map(book -> ResponseEntity.ok(book))
+                .orElse(ResponseEntity.notFound().build());
+    }
 
 
 
