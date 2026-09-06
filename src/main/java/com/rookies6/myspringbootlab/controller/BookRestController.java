3afbb82336c4c41339bb660937c71487d71f0d6b
jero-06 <jeeyoung345@gmail.com>
2026-09-06 14:47:16 +0900

    PostMapping

--- src/main/java/com/rookies6/myspringbootlab/controller/BookRestController.java
+++ src/main/java/com/rookies6/myspringbootlab/controller/BookRestController.java
@@ -1,5 +1,6 @@
 package com.rookies6.myspringbootlab.controller;
 
+import com.rookies6.myspringbootlab.entity.Book;
 import com.rookies6.myspringbootlab.repository.BookRepository;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,7 +14,7 @@ public class BookRestController {
         this.bookRepository = bookRepository;
     }
 
-    @PostMapping   // "새 도서 등록"이니 어떤 HTTP 메서드용 애노테이션이 필요할까요?
+    @PostMapping
     public Book createBook(@RequestBody Book book) {
         // 요청 본문(JSON)을 Book 객체로 받으려면 파라미터 앞에 어떤 애노테이션이 필요할까요?
         return bookRepository.save(book);   // book을 DB에 저장하고, 그 저장된 결과를 리턴하려면?
