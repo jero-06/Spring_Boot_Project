da76d597ea90886b9340e38fcc235ac19e6ddc98
jero-06 <jeeyoung345@gmail.com>
2026-09-06 14:53:06 +0900

    GetMapping

--- src/main/java/com/rookies6/myspringbootlab/controller/BookRestController.java
+++ src/main/java/com/rookies6/myspringbootlab/controller/BookRestController.java
@@ -4,6 +4,8 @@ import com.rookies6.myspringbootlab.entity.Book;
 import com.rookies6.myspringbootlab.repository.BookRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/books")
@@ -16,8 +18,12 @@ public class BookRestController {
 
     @PostMapping
     public Book createBook(@RequestBody Book book) {
-        // 요청 본문(JSON)을 Book 객체로 받으려면 파라미터 앞에 어떤 애노테이션이 필요할까요?
-        return bookRepository.save(book);   // book을 DB에 저장하고, 그 저장된 결과를 리턴하려면?
+                return bookRepository.save(book);
+    }
+
+    @GetMapping
+    public List<Book> getAllBook() {
+        return bookRepository.findAll();
     }
 
 
