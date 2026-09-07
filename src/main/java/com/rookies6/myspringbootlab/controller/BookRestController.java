5e713273bf771c27a6b9e0d0bdf3bd7152e0a221
jero-06 <jeeyoung345@gmail.com>
2026-09-08 00:35:44 +0900

    getUserByIsbn update

--- src/main/java/com/rookies6/myspringbootlab/controller/BookRestController.java
+++ src/main/java/com/rookies6/myspringbootlab/controller/BookRestController.java
@@ -1,7 +1,9 @@
 package com.rookies6.myspringbootlab.controller;
 
 import com.rookies6.myspringbootlab.entity.Book;
+import com.rookies6.myspringbootlab.exception.BusinessException;
 import com.rookies6.myspringbootlab.repository.BookRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,6 +38,15 @@ public class BookRestController {
         return bookRepository.findById(id)
                 .map(book -> ResponseEntity.ok(book))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    // ISBN으로 도서 조회
+    @GetMapping("/isbn/{isbn}")
+    public Book getUserByIsbn(@PathVariable String isbn) {
+        return bookRepository.findByIsbn(isbn)
+                .orElseThrow(() -> new BusinessException("책을 찾을 수 없습니다.", HttpStatus.404));
+        // Optional이 비어있을 때 그냥 기본값을 리턴하는 게 아니라, "예외를 던지는" 방식의 메서드가 있어요.
+        // findById 때 썼던 map/orElse와는 다른, orElse와 이름이 비슷한 메서드입니다. 뭘까요?
     }
 
 
