cbdf9151ae2d45a4fc2dc852ad3de5a5859f4c2e
jero-06 <jeeyoung345@gmail.com>
2026-09-09 01:22:10 +0900

    BookService add

---
+++ src/main/java/com/rookies6/myspringbootlab/service/BookService.java
@@ -0,0 +1,27 @@
+package com.rookies6.myspringbootlab.service;
+
+import com.rookies6.myspringbootlab.controller.dto.BookDTO;
+import com.rookies6.myspringbootlab.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public List<BookDTO.BookResponse> getAllBooks() {
+        return bookRepository.findAll()
+                .stream()
+                .map(book -> BookDTO.BookResponse.from(book))                    // Book 하나를 BookResponse로 변환하려면, 우리가 아까 만든 어떤 메서드를 쓰면 될까요?
+                .toList();
+    }
+
+}
