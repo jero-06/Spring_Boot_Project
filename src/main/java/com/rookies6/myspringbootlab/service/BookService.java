65825dea9487b8da880dab3f61b07c12be0e0f1e
jero-06 <jeeyoung345@gmail.com>
2026-09-09 01:59:13 +0900

    BookService update

--- src/main/java/com/rookies6/myspringbootlab/service/BookService.java
+++ src/main/java/com/rookies6/myspringbootlab/service/BookService.java
@@ -1,8 +1,13 @@
 package com.rookies6.myspringbootlab.service;
 
 import com.rookies6.myspringbootlab.dto.BookDTO;
+import com.rookies6.myspringbootlab.entity.Book;
+import com.rookies6.myspringbootlab.exception.BusinessException;
+import com.rookies6.myspringbootlab.exception.ErrorCode;
 import com.rookies6.myspringbootlab.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,11 +20,26 @@ public class BookService {
 
     private final BookRepository bookRepository;
 
+    // 모든 도서 목록 조회
     public List<BookDTO.BookResponse> getAllBooks() {
         return bookRepository.findAll()
                 .stream()
-                .map(book -> BookDTO.BookResponse.from(book))                    // Book 하나를 BookResponse로 변환하려면, 우리가 아까 만든 어떤 메서드를 쓰면 될까요?
+                .map(book -> BookDTO.BookResponse.from(book))
                 .toList();
+    }
+
+    // Id 조회
+    public BookDTO.BookResponse getBookById(Long id) {
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new BusinessException("해당 Id의 도서를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+        return BookDTO.BookResponse.from(book);
+    }
+
+    // Isbn 조회
+    public BookDTO.BookResponse getBookByIsbn(String isbn) {
+        Book book = bookRepository.findByIsbn(isbn)
+                .orElseThrow(() -> new BusinessException("해당 번호의 도서를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+        return BookDTO.BookResponse.from(book);
     }
 
 }
